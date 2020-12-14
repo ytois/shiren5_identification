f74c6c7e2975e8d226df032278886d61405c11ed
ytois <xxzoo200xx@gmail.com>
2020-12-14 21:10:14 +0900

    add material-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import logo from './logo.svg'
 import './App.css'
+import { Button } from '@material-ui/core'
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 function App() {
@@ -11,14 +12,9 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <Button color="primary" href="https://reactjs.org">
           Learn React
-        </a>
+        </Button>
       </header>
     </div>
   )
--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import { ThemeProvider } from '@material-ui/styles'
+import { CssBaseline, createMuiTheme } from '@material-ui/core'
+
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+  },
+})
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
